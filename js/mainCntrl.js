f27ac2962baac52b935d0f24cb242111646986ee
taryngabbert@gmail.com <taryngabbert@gmail.com>
2016-06-03 16:58:28 -0500

    first commit

---
+++ js/mainCntrl.js
@@ -0,0 +1,25 @@
+angular.module("app")
+.controller("mainCntrl", function($scope, $http, service) {
+// INSERT YOUR CODE BETWEEN THE BRACKETS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ js/service.js
@@ -0,0 +1,17 @@
+angular.module("app")
+.service("service", function($http) {
+//  INSERT YOUR CODE HERE
+//  using this.funtionName will allow easy calling in your mainCntrl
+
+
+
+
+
+
+
+
+
+
+
+
+});
